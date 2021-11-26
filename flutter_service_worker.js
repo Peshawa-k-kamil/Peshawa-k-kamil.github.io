5a2b521e13a7d0ca1d68ef5b3067e3325a922167
ideaadegroup <peshawa.k.kamil@gmail.com>
2021-11-26 13:55:32 +0300

    some change

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,9 +27,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "93a7e26be2203f3a3a8f66306a95cf93",
-"/": "93a7e26be2203f3a3a8f66306a95cf93",
-"main.dart.js": "588a61d035f78e5d13c15e3d1526d819",
+"index.html": "27d2bbf8b7d734a848b97611858df243",
+"/": "27d2bbf8b7d734a848b97611858df243",
+"main.dart.js": "2daf1beb692915be4757418e500edb8b",
 "manifest.json": "222d22e9730a5020e3cc3691d302e13c",
 "version.json": "a46c16b7d4e531e94ee8f53adf199d5f"
 };
