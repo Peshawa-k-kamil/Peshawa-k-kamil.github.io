2b05a185cf8217c0c7db30f5dd1e60066395affd
ideaadegroup <peshawa.k.kamil@gmail.com>
2021-12-04 10:02:04 +0300

    better responsive

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -39,9 +39,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "780574346967c8804cd28a61cd01c5ea",
-"/": "780574346967c8804cd28a61cd01c5ea",
-"main.dart.js": "a209567a431a71f9ffbefd09f1cea9a4",
+"index.html": "54198d12027f1d8b1fe27aa7b0073dc6",
+"/": "54198d12027f1d8b1fe27aa7b0073dc6",
+"main.dart.js": "6ce03aa3eb6daf7e084fc5af642851e3",
 "manifest.json": "222d22e9730a5020e3cc3691d302e13c",
 "version.json": "a46c16b7d4e531e94ee8f53adf199d5f"
 };
