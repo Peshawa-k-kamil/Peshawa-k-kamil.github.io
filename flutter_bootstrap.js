e4d7cb45ec6166d601937efbdcfbfbed2f104f55
peshawa-k-kamil <peshawa.k.kamil@gmail.com>
2024-06-25 22:10:35 +0300

    3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2608090583"
+    serviceWorkerVersion: "1706275444"
   }
 });
